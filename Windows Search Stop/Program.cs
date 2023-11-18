0b39e0abbbd068e50301986f360f29bc5ffaaee3
Doctor023 <109444069+Doctor023@users.noreply.github.com>
2023-11-18 16:26:25 +0300

    Добавьте файлы проекта.

---
+++ Windows Search Stop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Windows_Search_Stop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "net stop \"Windows seach\"";
+
+        ProcessStartInfo processStartInfo = new ProcessStartInfo();
+        processStartInfo.FileName = "powershell.exe"; 
+        processStartInfo.Arguments = $"-Command {command}"; 
+        processStartInfo.RedirectStandardOutput = true;
+        processStartInfo.UseShellExecute = false;
+        processStartInfo.CreateNoWindow = true;
+
+        // Запускаем процесс
+        Process process = new Process();
+        process.StartInfo = processStartInfo;
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+
+        Console.WriteLine(output);
+
+        process.WaitForExit();
+        }
+    }
+}
